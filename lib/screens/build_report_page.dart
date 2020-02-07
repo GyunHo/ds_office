ce62e356880403f9060796a172f50fa167231b7a
GyunHo <48146563+GyunHo@users.noreply.github.com>
2020-02-07 16:57:12 +0900

    start build page

--- lib/screens/build_report_page.dart
+++ lib/screens/build_report_page.dart
@@ -1,6 +1,6 @@
 import 'package:kf_drawer/kf_drawer.dart';
 import 'package:flutter/material.dart';
-
+import 'package:find_dropdown/find_dropdown.dart';
 class BuildReport extends KFDrawerContent {
   @override
   _BuildReportState createState() => _BuildReportState();
@@ -11,6 +11,7 @@ class _BuildReportState extends State<BuildReport> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
           '시설내역서',
           style: TextStyle(color: Colors.white),
@@ -19,6 +20,28 @@ class _BuildReportState extends State<BuildReport> {
         leading: IconButton(
           onPressed: widget.onMenuPressed,
           icon: Icon(Icons.menu),
+        ),
+
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title:  FindDropdown<String>(onChanged: (val){},items:[] ,
+
+                    searchBoxDecoration: InputDecoration(
+                      hintText: "자재 검색"
+                    ),
+
+                  ),
+                ),
+              )
+
+            ],
+          ),
         ),
       ),
     );
